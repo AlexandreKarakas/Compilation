134db048ad9624cf701e7b37592dc490fc8daa4a
Alexandre KARAKAS <alexandre.karakas@gmail.com>
2020-04-15 17:36:34 +0200

    ajout fonction parcours

--- TP_C_exercice2/TP.c
+++ TP_C_exercice2/TP.c
@@ -5,11 +5,29 @@
 #include <stdlib.h>
 #include "AST.c"
 
+float parcours(struct ExpressionA* ast);
+
 int main(void)
 {
   struct ExpressionA* ast = (struct  ExpressionA*)malloc( sizeof( struct  ExpressionA ) );
-
+  float val;
   yyparse(ast);
+  val = parcours(ast);
+  fprintf(stderr, "val: %lf \n", val );
+}
 
-  fprintf(stderr, "symbol principal: %c \n", ast->sym );
+
+float parcours(struct ExpressionA* ast){
+	if (!ast) 
+		return 0;
+  switch (ast->sym)
+  {
+    case '+' : return parcours(ast->left) + parcours(ast->right);
+    case '-' : return parcours(ast->left) - parcours(ast->right);
+    case '/' : return parcours(ast->left) / parcours(ast->right);
+    case '*' : return parcours(ast->left) * parcours(ast->right);
+    case '%' : return (float)(((int)parcours(ast->left)) % ((int)parcours(ast->right)));
+    default : return ast->val;
+  }
+  return 0;
 }
