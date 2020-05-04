065a1425b6fe052f8996ece7ac7c4995755213b2
Nady Saddik <nady01@hotmail.fr>
2020-05-04 17:56:27 +0200

    Correction + opti

---
+++ TP2/Ex2/AST.c
@@ -0,0 +1,15 @@
+
+#include<stdlib.h>
+
+
+
+struct ExpressionA {
+  char sym;                    /* '+', '-', '/' ou '*' , quelconque si terme*/
+  struct ExpressionA* left;    /* null si nombre */
+  struct ExpressionA* middle;  /* null si nombre */
+  struct ExpressionA* right;   /* null si nombre ou neg */
+  int val;                     /* quelconque si <> nombre */
+  int taille;                  /* taille du noeud */
+};
+
+
---
+++ TP2/Ex2/code.jsm
@@ -0,0 +1,8 @@
+CstRe 1
+CstRe 3
+Equal
+ConJmp 3
+CstRe 2
+Jump 2
+CstRe 5
+Halt
