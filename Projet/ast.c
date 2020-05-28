eb0f045eaa3f7ae9753ddf0c604364e705fd8fa4
Nady Saddik <nady01@hotmail.fr>
2020-05-28 09:53:35 +0200

    Dernière version du projet + corrections
    Inclus fragment 0, 1 et 2 et un fichier code.txt pour tester

---
+++ Projet/ast.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "ast.h"
+#include <stdio.h>
+
+Node* newAST(Node* command, Node* program){
+    Node* node;
+    if(!(node = (Node*) malloc(sizeof(Node)))) return NULL;
+
+    node->type = ROOT_NODE;
+    node->root.command = command;
+    node->root.program = program;
+
+    return node;
+}
+
+Node* newOperation(int type, int nops, ...){
+  Node* node;
+  va_list ap;
+
+  if(!(node = (Node*) malloc(sizeof(Node)))) return NULL;
+
+  node->type = OPER_NODE;
+  node->oper.type = type;
+  node->oper.nops = nops;
+
+  va_start(ap, nops);
+  for(int i=0;i<nops;i++){
+    node->oper.childs[i] = va_arg(ap, Node*);
+  }
+  va_end(ap);
+
+  return node;
+}
+
+Node* newConst(int n){
+  Node* node;
+  if(!(node = (Node*) malloc(sizeof(Node)))) return NULL;
+
+  node->type = CONST_NODE;
+  node->cst.valueType = INT_VALUE;
+  node->cst.iValue = n;
+
+  return node;
+}
+
+Node* newString(char* string){
+    Node* node;
+    if(!(node = (Node*) malloc(sizeof(Node)))) return NULL;
+
+    node->type = CONST_NODE;
+    node->cst.valueType = STRING_VALUE;
+    node->cst.sValue = string;
+
+    return node;
+}
+
+Node* newIdent(char* id){
+  Node* node;
+  if(!(node = (Node*) malloc(sizeof(Node)))) return NULL;
+
+  node->type = ID_NODE;
+  node->id.name = id;
+
+  return node;
+}
---
+++ Projet/ast.h
@@ -0,0 +1,57 @@
+#ifndef AST_H_
+#define AST_H_
+
+/* ----- NODE TYPES ----- */
+
+typedef enum Node_type {
+    CONST_NODE, ID_NODE, OPER_NODE, ROOT_NODE
+} Node_type;
+
+typedef enum Value_type {
+    STRING_VALUE, INT_VALUE
+} Value_type;
+
+typedef struct {
+    Value_type valueType;
+
+    union{
+        int iValue;
+        char* sValue;
+    };
+} Const_node;
+
+typedef struct {
+    char* name;
+} ID_node;
+
+typedef struct {
+    int type;
+    int nops;
+    struct Node* childs[4];
+} Oper_node;
+
+typedef struct {
+    struct Node* command;
+    struct Node* program;
+} Root_node;
+
+typedef struct Node Node;
+struct Node {
+  Node_type type; // Type du noeud (ex : IF)
+  int height;   // Taille du noeud
+
+  union {
+    Root_node root;
+    Const_node cst;
+    ID_node id;
+    Oper_node oper;
+  };
+};
+
+Node* newAST(Node*, Node*);
+Node* newOperation(int, int, ...);
+Node* newConst(int);
+Node* newString(char*);
+Node* newIdent(char*);
+
+#endif /* AST_H_ */
